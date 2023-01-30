278f00ad8afa1cffb1abfa2bcfe03e33d348ea72
ChidoMunyanyi <119809704+ChidoMunyanyi@users.noreply.github.com>
2023-01-30 02:02:39 +0000

    Added Questions Variable

---
+++ assets/questions.js
@@ -0,0 +1,22 @@
+let questions = [
+    {
+        question1: "What is JavasSript?";
+        options: ["An indie band", "Who actually knows", "A programming language", "A pain in my ***"];
+        answer: "A programming language";
+    }
+    {
+        question2: "Who created JavaScript?";
+        options: ["Nicholas Cage", "Brendan Eich", "Darth Vader", "Elliot Hunch"];
+        answer: "Brendan Eich"
+    }
+    {
+        question3: "What is JavaScript used for?";
+        options: ["To create dynamic and interactive web content", "To structure a web page", "To induce delirium", "To communicate with other planets"];
+        answers: "To create dynamic and interactive web content";
+    }
+    {
+        question4: "What percentage of the world uses JavaScript?";
+        options: ["30", "56", "100", "97"];
+        answers: "97";
+    }
+]
--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,13 @@
 const startButton = document.getElementById('start');
 const timer = document.getElementById('time')
+
+//startButton.addEventListener("click", function() {
+
+   // const timer = 60;
+
+ //   var timeInterval = setInterval(function () {
+   //     if 
+   // }
+ //   )
+//})
+startButton.onclick = ()
